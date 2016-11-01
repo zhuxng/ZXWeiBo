1972ad016b177ab5d8e11f4e0ed7e4ffd319d73c
zhuxng <979519278@qq.com>
2016-11-01 20:56:59 +0800

    VC
    添加第一个VC

---
+++ ZXWeiBo/MyViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MyViewController.swift
+//  ZXWeiBo
+//
+//  Created by 朱星 on 2016/11/1.
+//  Copyright © 2016年 zhuxing. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    
+    
+    
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
